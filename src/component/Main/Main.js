0ed8670c72929b0fdbc332024158278a50641eb8
saebom <boma91@gmail.com>
2021-07-04 12:14:03 +0900

    Add: main 페이지 추천매물 ㅜ가

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,19 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { useHistory } from 'react-router';
 import './Main.scss';
 
-const Main = () => {
+const Main = (props) => {
+    console.log(props.state)
 
     let history = useHistory();
 
     return (
         <div className="Main">
-            <h1>어떤 방을 찾으세요?</h1>
-            <button onClick={() => {
-                history.push('./rooms')
-            }}>방 구경가기 👀</button>            
+            <div className="introduce">
+                <h1>어떤 방을 찾으세요?</h1>
+                <button onClick={() => {
+                    history.push('./rooms')
+                }}>방 구경가기 👀</button>
+            </div>
+            <div className="recommend">
+                <p>방방의 추천 매물</p>
+                <ul>
+                    {
+                        props.state.map((a,i) => {
+                            return (
+                                <li><img src={props.state[i].thumbnail} /></li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         </div>
     );
 };
 
-export default Main;
+function Props(state) {
+    return {
+        state: state
+    }
+}
+export default connect(Props)(Main);
